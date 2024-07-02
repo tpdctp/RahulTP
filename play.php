b2c538c91d9a37896b68fe6bf380666b69cffd3a
officialmyweb <adarshmishra.pj@gmail.com>
2024-07-02 08:05:37 +0530

    Update play.php

--- play.php
+++ play.php
@@ -21,7 +21,7 @@ $key = $selectedChannel['key'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tataplay Live</title>
+    <title>Criccoder - Tataplay</title>
     <link rel="stylesheet" href="./player.css">
     <style>
         #player {
@@ -128,6 +128,8 @@ playerInstance.on("ready", function () {
         };
     });
 });
+
+    if (confirm("Join Our Telegram Channel @criccoder")) { window.location.href = "https://t.me/criccoder"; }
 </script>
 </body>
 </html>
